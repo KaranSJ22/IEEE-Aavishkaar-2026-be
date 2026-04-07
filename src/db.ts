f020ae13eef6bd06f14cb2b403646aeaac173537
Shivesh Tiwari <shiveshtiwari0@gmail.com>
2026-04-07 11:51:07 +0530

    deployment issue fix with mongodb connection

--- src/db.ts
+++ src/db.ts
@@ -2,17 +2,17 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
-
-const MONGO_URL = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/techfest26";
+const MONGO_URL =
+  process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/techfest26";
 
 export const mongodbClient = {
-    init: async () => {
-        try {
-            await mongoose.connect(MONGO_URL);
-            console.log("✅ Successfully connected to MongoDB");
-        } catch (error) {
-            console.error("❌ MongoDB connection error:", error);
-            process.exit(1);
-        }
+  init: async () => {
+    try {
+      await mongoose.connect(MONGO_URL);
+      console.log("✅ Successfully connected to MongoDB");
+    } catch (error) {
+      console.error("❌ MongoDB connection error:", error);
+      process.exit(1);
     }
+  },
 };
